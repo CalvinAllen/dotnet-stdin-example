e876ca8f6296e98c00c8602354667c9fa47c0620
Calvin A. Allen <calvin.allen@outlook.com>
2022-03-27 11:40:25 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# .NET Standard Input Example
+
+This sample is built using .NET 6
+
+## Examples
+
+Given a terminal with the current directory set as the output directory of the build -
+
+* cat ..\\..\\..\\..\\..\\input\\moby_dick.txt | .\CodingWithCalvin.NETStandardInExample.Console.exe
+* cat ..\\..\\..\\..\\..\\input\\frankenstein.txt | .\CodingWithCalvin.NETStandardInExample.Console.exe
---
+++ src/CodingWithCalvin.NETStandardInExample.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CodingWithCalvin.NETStandardInExample.Console;
+
+public class Program
+{
+    private static async Task Main(string[] args)
+    {
+        if (System.Console.IsInputRedirected)
+        {
+            List<string> fileLines = new();
+
+            while (await System.Console.In.ReadLineAsync() is { } pipedInput)
+            {
+                fileLines.Add(pipedInput);
+            }
+
+            fileLines.ForEach(line => System.Console.WriteLine(line));
+        }
+        else
+        {
+            System.Console.WriteLine("Do something here with args, if you wanted to support both");
+        }
+    }
+}
